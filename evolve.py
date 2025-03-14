e1109500b139b7e710184cf8a758193765692fb0
Lukas Plevac <iplevac@fit.vutbr.cz>
2025-03-14 12:20:33 +0100

    Evolve fix

--- evolve.py
+++ evolve.py
@@ -4,10 +4,10 @@ import json
 RUNS = 30
 GENS = 100000
 Files = ["claBlock1.v", "claBlock2.v", "claBlock3.v", "claBlock4.v"]
-BEST_SIZE = None
 outFinalFIle = "evo_"
 
 for inFile in Files:
+    BEST_SIZE = None
     for run in range(RUNS):
         outFIle = outFinalFIle + ".tmp" 
         os.system("yosys/yosys -m cgploss.so -p \"read_verilog {}; techmap; cgploss -representation=mig -power_accuracy_ratio=1 -max_abs_error=0 -max_one_error=0 -generations={} -save_final={} -keep_delay\"".format(inFile, GENS, outFIle))
@@ -20,7 +20,7 @@ for inFile in Files:
             os.system("mv {} {}_{}.json".format(outFIle, outFinalFIle, inFile))
             BEST_SIZE = len(chrom["chromosome"])
 
-print()
-print("Done bets ind is from {} GATES".format(BEST_SIZE))
+    print()
+    print("Done bets ind is from {} GATES".format(BEST_SIZE))
 
 
