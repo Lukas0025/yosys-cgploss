37464e48b50b71fa8ebc8cfb7d31bd763b07da3c
Lukas Plevac <iplevac@fit.vutbr.cz>
2025-04-04 10:41:12 +0200

    Remove testing verologs

--- claBlock1.v
+++
@@ -1,5 +0,0 @@
-module top( g0 , p0, cin, cout);
-  input g0, p0, cin;
-  output cout;
-  assign cout = g0 | (p0 & cin);
-endmodule
--- claBlock3.v
+++
@@ -1,5 +0,0 @@
-module top( g0 , p0, g1, p1, g2, p2, cin, cout);
-  input g0, p0, g1, p1, g2, p2, cin;
-  output cout;
-  assign cout = g2 | (p2 & g1) | (p2 & p1 & g0) | (p2 & p1 & p0 & cin);
-endmodule
